2351d881c747b76086830e07b9a3b30807a5f550
JuanQP <quinterosp.juan@gmail.com>
2022-10-06 20:07:54 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Card, Center, Container, Text } from "@mantine/core";
+
+function App() {
+
+  return (
+    <Center>
+      <Container py="sm">
+        <Card shadow="sm">
+          <Text>Hola mundo!</Text>
+        </Card>
+      </Container>
+    </Center>
+  )
+}
+
+export default App
